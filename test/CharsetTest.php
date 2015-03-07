a58f2c036e8f220c0bcfdd6312ef45131557a490
Andrew Konoval <akonoval@softserveinc.com>
2015-03-07 13:46:18 +0200

    Indentations fixes.

--- test/CharsetTest.php
+++ test/CharsetTest.php
@@ -6,58 +6,59 @@ require_once 'test/config.php';
 /**
  * Test class for testing charset stting
  */
+class CharsetTest extends PHPUnit_Framework_testCase
+{
+    var $params = array(
+        'deployment_zone_id' => 'test',
+        'product_id' => 'test',
+        'cloud_key' => 'test',
+        'staging' => TRUE,
+    );
 
-class CharsetTest extends PHPUnit_Framework_testCase {
+    // Use reflection to test private methods
+    protected static function getMethod($name)
+    {
+        $class = new ReflectionClass('Base');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
 
-	var $params = array(
-		'deployment_zone_id' => 'test',
-		'product_id'		 => 'test',
-		'cloud_key'			 => 'test',
-		'staging'			 => TRUE,
-	);
+    /**
+     * Test charset.
+     */
+    public function testCharsetEncode()
+    {
 
-	// Use reflection to test private methods
-	protected static function getMethod($name) {
-		$class = new ReflectionClass('Base');
-		$method = $class->getMethod($name);
-		$method->setAccessible(true);
-		return $method;
-	}
+        $params['charset'] = 'Windows-1251';
 
-	/**
-	 * Test charset.
-	 */
-	public function testCharsetEncode() {
+        $charsetEncode = self::getMethod('_charsetEncode');
 
-		$params['charset'] = 'Windows-1251';
+        $obj = new Base($params);
+        $a = $charsetEncode->invokeArgs($obj, array("This is the Euro symbol 'в‚¬'"));
+        $this->assertEquals("This is the Euro symbol '€'", $a);
 
-		$charsetEncode = self::getMethod('_charsetEncode');
+        $b = $charsetEncode->invokeArgs($obj, array("РљРёСЂРёР»Р»РёС†Р°"));
+        $this->assertEquals("Кириллица", $b);
+    }
 
-		$obj = new Base($params);
-		$a = $charsetEncode->invokeArgs($obj, array("This is the Euro symbol 'в‚¬'"));
-		$this->assertEquals("This is the Euro symbol '€'", $a);
+    public function testCharsetCheck()
+    {
 
-		$b = $charsetEncode->invokeArgs($obj, array("РљРёСЂРёР»Р»РёС†Р°"));
-		$this->assertEquals("Кириллица", $b);
-	}
+        $params['charset'] = 'NOT_EXISTING_CHARSET';
 
-	public function testCharsetCheck() {
+        $checkCharset = self::getMethod('_checkCharset');
 
-		$params['charset'] = 'NOT_EXISTING_CHARSET';
+        // Check for set to default
+        $obj = new Base($params);
+        $checkCharset->invokeArgs($obj, array("Hello world!"));
+        $this->assertEquals("UTF-8", $obj->config['charset']);
 
-		$checkCharset = self::getMethod('_checkCharset');
+        // Check correct charset
+        $params['charset'] = 'UTF-16';
+        $obj = new Base($params);
+        $checkCharset->invokeArgs($obj, array("Hello world!"));
+        $this->assertEquals("UTF-16", $obj->config['charset']);
+    }
 
-		// Check for set to default
-		$obj = new Base($params);
-		$checkCharset->invokeArgs($obj, array("Hello world!"));
-		$this->assertEquals("UTF-8", $obj->config['charset']);
-
-		// Check correct charset
-		$params['charset'] = 'UTF-16';
-		$obj = new Base($params);
-		$checkCharset->invokeArgs($obj, array("Hello world!"));
-		$this->assertEquals("UTF-16", $obj->config['charset']);
-	}
 }
-
-
--- test/StoryTest.php
+++ test/StoryTest.php
@@ -6,30 +6,32 @@ require_once 'test/config.php';
 /**
  * Test class to test Stories.
  */
-class StoryTest extends PHPUnit_Framework_testCase {
+class StoryTest extends PHPUnit_Framework_testCase
+{
+    public $cloud_key = 'myshco-3e3001e88d9c32d19a17cafacb81bec7';
+    public $deployment_zone_id = '9344';
+    public $product_id = '12345';
 
-	public $cloud_key          = 'myshco-3e3001e88d9c32d19a17cafacb81bec7';
-	public $deployment_zone_id = '9344';
-	public $product_id			= '12345';
+    /**
+     * Test stories.
+     */
+    public function testStory()
+    {
+        // to force is_bot mode
+        $_SERVER['HTTP_USER_AGENT'] = "google";
 
-	/**
-	 * Test stories.
-	 */
-	public function testStory () {
-		// to force is_bot mode
-		$_SERVER['HTTP_USER_AGENT'] = "google";
+        $bv = new BV(array(
+            'deployment_zone_id' => $this->deployment_zone_id,
+            'product_id' => $this->product_id,
+            'cloud_key' => $this->cloud_key,
+            'staging' => TRUE,
+        ));
 
-		$bv = new BV(array(
-			'deployment_zone_id' => $this->deployment_zone_id,
-			'product_id'		 => $this->product_id,
-			'cloud_key'			 => $this->cloud_key,
-			'staging'			 => TRUE,
-		));
+        $content = $bv->stories->getContent();
 
-		$content = $bv->stories->getContent();
+        $this->assertNotNull($content, "There should be content to proceed further assertion!!");
+        $this->assertNotContains("Error - ", $content, "There should be valid content");
+        $this->assertNotContains("HTTP status code of", $content, "There should be valid content");
+    }
 
-		$this->assertNotNull($content, "There should be content to proceed further assertion!!");
-		$this->assertNotContains("Error - ", $content, "There should be valid content");
-		$this->assertNotContains("HTTP status code of", $content, "There should be valid content");
-	}
 }
