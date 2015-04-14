9996bc62fee0d5f86ebe648077f48aea9c6d508f
Andrew Konoval <akonoval@softserveinc.com>
2015-04-14 10:47:51 +0300

    SEO-625: Create support for Mock Data for PHP Unit tests
    [close #16]

--- test/StoriesTest.php
+++ test/StoriesTest.php
@@ -8,31 +8,58 @@ require_once 'test/config.php';
  */
 class StoriesImplementingTest extends PHPUnit_Framework_testCase
 {
-    public $cloud_key = 'myshco-3e3001e88d9c32d19a17cafacb81bec7';
-    public $deployment_zone_id = '9344';
-    public $product_id = '12345';
+    var $params = array(
+        'bv_root_folder' => 'test',
+        'subject_id' => 'test',
+        'cloud_key' => 'test',
+        'staging' => FALSE,
+        'testing' => FALSE,
+        'crawler_agent_pattern' => 'msnbot|google|teoma|bingbot|yandexbot|yahoo',
+        'ssl_enabled' => FALSE,
+        'content_type' => 'product',
+        'subject_type' => 'category',
+        'execution_timeout' => 500,
+        'execution_timeout_bot' => 2000,
+        'load_seo_files_locally' => FALSE,
+        'seo_sdk_enabled' => TRUE,
+        'proxy_host' => '',
+        'proxy_port' => '',
+        'charset' => 'UTF-8',
+        'base_url' => '/base/url',
+        'page_url' => '/page/url?debug=true',
+        'include_display_integration_code' => TRUE,
+    );
 
-    /**
-     * Test stories.
-     */
-    public function testStory()
+    public function testStories()
     {
         // to force is_bot mode
-        $_SERVER['HTTP_USER_AGENT'] = "google";
+        $_SERVER['HTTP_USER_AGENT'] = 'google';
 
-        $bv = new BV(array(
-            'bv_root_folder' => $this->deployment_zone_id,
-            'subject_id' => $this->product_id,
-            'cloud_key' => $this->cloud_key,
-            'execution_timeout_bot' => 5000,
-            'staging' => TRUE,
-        ));
+        $obj = $this->getMockBuilder('Stories')
+                ->setConstructorArgs(array($this->params))
+                ->setMethods(['curlError', 'curlErrorNo', 'curlExecute', 'curlInfo'])
+                ->getMock();
+        $obj->expects($this->any())
+                ->method('curlError')
+                ->will($this->returnValue('No errors'));
+        $obj->expects($this->any())
+                ->method('curlErrorNo')
+                ->will($this->returnValue(0));
+        $obj->expects($this->any())
+                ->method('curlExecute')
+                ->will($this->returnValue('<div id="BVRRContainer">
+						<p>Mock content for unit tests.</p>
+					</div>'));
+        $obj->expects($this->any())
+                ->method('curlInfo')
+                ->will($this->returnValue(array('http_code' => 200, 'total_time' => 100)));
 
-        $content = $bv->stories->getContent();
+        $res = $obj->getContent();
 
-        $this->assertNotNull($content, "There should be content to proceed further assertion!!");
-        $this->assertNotContains("Error - ", $content, "There should be valid content");
-        $this->assertNotContains("HTTP status code of", $content, "There should be valid content");
+        $this->assertNotNull($res);
+        $this->assertContains('Mock content for unit tests', $res);
+        $this->assertContains('STORIES, PRODUCT', $res);
+        $this->assertNotContains('Error - ', $res);
     }
 
 }
